0078198497f6f9b7df4065e2c1312609185ead0b
Lakindu Hewawasam <lakindu2002@live.com>
2021-08-21 21:29:01 +0530

    updated comments

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const requiredPort = process.env.DEPLOY_PORT; //change to a vacant port if 5000 
 const configuredRoutes = require("./routes/routes"); //require the routes created for the API
 const interceptors = require("./interceptors/auth.interceptor");
 
+//setup a basic auth interceptor to intercept every request for basic auth
 app.use(interceptors.authenticateBangerClient);
 
 app.get("/", async (req, res) => {
