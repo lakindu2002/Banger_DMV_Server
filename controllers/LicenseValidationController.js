33f4c440f80f1370ce3d391671787f19848030fe
Lakindu Hewawasam <lakindu2002@live.com>
2021-08-22 10:01:38 +0530

    add res status for csv fetch

--- controllers/LicenseValidationController.js
+++ controllers/LicenseValidationController.js
@@ -1,4 +1,5 @@
 exports.fetchCsv = (req, res) => {
     //fetch the csv and send to the banger server to download
+    res.status(200);
     res.download("." + "/files/license_list_cb007787.csv");
 }
