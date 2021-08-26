57b1a5220b6146472419638e0d11eb56406f08d4
Lakindu Hewawasam <lakindu2002@live.com>
2021-08-26 10:50:32 +0530

    implemented the registration number validation on auth

--- files/in.memory.db.js
+++ files/in.memory.db.js
@@ -1,7 +1,14 @@
 //sample database to identify users that have access to the api
+//testing purposes - password = banger@admin
 exports.apiAccess = [
     {
+        username: "dmv@admin",
+        password: "$2b$10$w3r1gSfBfHlUwqzOnsdhkuyMWeIEJRyB.oFEvsmonbml87J5vzlDG",
+        registration:"JA798QRR"
+    },
+    {
         username: "banger@admin",
-        password: "$2b$10$w3r1gSfBfHlUwqzOnsdhkuyMWeIEJRyB.oFEvsmonbml87J5vzlDG"
+        password: "$2b$10$w3r1gSfBfHlUwqzOnsdhkuyMWeIEJRyB.oFEvsmonbml87J5vzlDG",
+        registration:"UJ267BAA"
     }
 ]
--- interceptors/auth.interceptor.js
+++ interceptors/auth.interceptor.js
@@ -7,11 +7,12 @@ exports.authenticateBangerClient = async (req, res) => {
     //secured using basic auth
     //Authorization Header = Basic Base64(username:password)
     //sample - Basic MTIzMTIzOjEyMzEyMzEyMw==
-    const passedInUser = auth(req);
+    const passedInUser = auth(req); //pass into the basic auth to decode from base 64 to java obj with name & password properties.
+    const registrationNumber = req.headers.registration //retrieve the registration number from request header
 
     if (passedInUser) {
         //valid user from basic auth
-        validationObj = await authenticateUser(passedInUser.name, passedInUser.pass); //authenticate user by checking access list
+        validationObj = await authenticateUser(passedInUser.name, passedInUser.pass, registrationNumber); //authenticate user by checking access list
         if (validationObj.code === 401) {
             //not authenticated
             res.status(401).json({
@@ -28,7 +29,7 @@ exports.authenticateBangerClient = async (req, res) => {
     }
 }
 
-const authenticateUser = async (username, providedPassword) => {
+const authenticateUser = async (username, providedPassword, registrationNumber) => {
     //check if username exists
     let user;
     apiAccess.forEach((eachClient) => {
@@ -40,7 +41,10 @@ const authenticateUser = async (username, providedPassword) => {
     if (user) {
         //check if entered password matches the password for client
         const isAuthenticated = await bcrypt.compare(providedPassword, user.password);
-        if (isAuthenticated === true) {
+        //check if registration number for user is same as header passed
+        const isRegistrationNumberValid = user.registration === registrationNumber ? true : false;
+        if (isAuthenticated === true && isRegistrationNumberValid) {
+            //is username and password match and the registration number belongs to the user, authenticate.
             return {
                 code: 200,
                 message: "authenticated"
@@ -48,7 +52,7 @@ const authenticateUser = async (username, providedPassword) => {
         } else {
             return {
                 code: 401,
-                message: "invalid username or password"
+                message: "invalid username or password or registration number"
             }
         }
     } else {
